d3198a73efdb16a865fd030cd98112b38d4175d5
LMR589281 <46445319+LMR589281@users.noreply.github.com>
2019-11-15 15:24:25 +0000

    fixed scripts

---
+++ Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+
+    public Vector2 speed = new Vector2(0, 0);
+
+    private Vector2 movement = new Vector2(1, 1);
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        movement = new Vector2(
+            speed.x * inputX,
+            speed.y * inputY);
+
+        if (Input.GetKeyDown("space")) //and check if on around
+        {
+            //transform.Translate(Vector3.up * 260 * Time.deltaTime, Space.World);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 4000));
+        }
+
+    }
+    void FixedUpdate()
+    {
+        // 5 - Move the game object
+        GetComponent<Rigidbody2D>().velocity = movement;
+        //rigidbody2D.AddForce(movement);
+
+    }
+}
---
+++ Scripts/TouchingFire.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class TouchingFire : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+}
