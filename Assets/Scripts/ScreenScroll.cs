1048089692e830488c00f254b3c05f117c274983
LMR589281 <46445319+LMR589281@users.noreply.github.com>
2019-11-19 10:59:16 +0000

    fixed scene not loading error

---
+++ Assets/Scripts/ScreenScroll.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenScroll : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        // Move the object upward in world space 1 unit/second.
+        transform.Translate(Vector3.left * Time.deltaTime, Space.World);
+    }
+}
