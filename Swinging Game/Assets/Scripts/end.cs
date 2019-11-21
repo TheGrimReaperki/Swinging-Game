24e0dc8a553af3ef3a19c2e7de72cde659e19692
Luke Ryan <lukeryan1@virginmedia.com>
2019-11-21 14:54:45 +0000

    added an end to the level

---
+++ Swinging Game/Assets/Scripts/end.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class end : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("test");
+        //SceneManager.LoadScene(3);
+    }
+}
